f16b17246e1f47dabd911bacba89e2a69122f70e
Barbaranis <barbara.champigny@gmail.com>
2025-11-25 09:55:36 +0100

    firebaseAdmin.js

--- backend/src/config/firebaseAdmin.js
+++ backend/src/config/firebaseAdmin.js
@@ -4,7 +4,19 @@
 
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require('../../serviceAccountKey.json'); 
+let serviceAccount;
+
+// En production (Render) : on lit le fichier secret
+if (process.env.FIREBASE_KEY_PATH) {
+  const keyPath = process.env.FIREBASE_KEY_PATH;
+  serviceAccount = require(keyPath);
+} else {
+  // En local : on continue d'utiliser le fichier classique NON poussé sur GitHub
+  const localPath = path.join(__dirname, '../../serviceAccountKey.json');
+  serviceAccount = require(localPath);
+}
+
+
 
 
 // 🔐 Initialisation de Firebase Admin SDK
