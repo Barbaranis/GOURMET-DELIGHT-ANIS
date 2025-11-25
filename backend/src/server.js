50e753fb5a4c32bcea08c830aff26fee0c8628c9
Barbaranis <barbara.champigny@gmail.com>
2025-11-25 14:31:08 +0100

    server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,7 +32,7 @@ const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
   'https://gourmetdeligh.netlify.app',
-  'https://gourmet-delight.netlify.app'
+  'https://gourmetdeligh.netlify.app'
 ];
 
 app.use(
